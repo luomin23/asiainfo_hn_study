5a7e401a1bb5d466b24742b936be9de0984a3faf
Jshihao <819779737@qq.com>
2017-07-28 18:12:36 +0800

    7-28

---
+++ src/main/java/com/asiainfo/chapter1/Jiashihao/JUnit/C1.java
@@ -0,0 +1,19 @@
+package com.asiainfo.chapter1.Jiashihao.JUnit;
+
+/**
+ * Created by lenovo on 2017/7/28.
+ */
+public class C1 {
+    public int add(int a,int b){
+        return a + b ;
+    }
+    public int subtract(int a, int b){
+        return a - b;
+    }
+    public int multiply(int a, int b){
+        return a * b ;
+    }
+    public int divide(int a, int b){
+        return a / b;
+    }
+}
