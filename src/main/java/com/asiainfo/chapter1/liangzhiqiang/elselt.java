5f10b31b9f8cc22bae8c9da5b7f113306b17b1c3
liangzhiqiang3 <546665217@qq.com>
2017-07-28 18:46:16 +0800

    lzq

--- src/main/java/com/asiainfo/chapter1/liangzhiqiang/elselt.java
+++ src/main/java/com/asiainfo/chapter1/liangzhiqiang/elselt.java
@@ -4,16 +4,15 @@ package com.asiainfo.chapter1.liangzhiqiang;
  */
 
 public class elselt {
-    public static void main(String [] arga){
-        double d=Math.random();//double类型，大小在【0.1】区间内
-        int i=(int)(d*5);//强制转换类型。大小区间【0.4】，如0.12*5=0.6.则转为int型后取整值为0
+    public static void main(String[] arga) {
+        double d = Math.random();//double类型，大小在【0.1】区间内
+        int i = (int) (d * 5);//强制转换类型。大小区间【0.4】，如0.12*5=0.6.则转为int型后取整值为0
         System.out.println(i);
-        if (i>3){
+        if (i > 3) {
             System.out.println("这是一个大数");
-        }
-        else {
+        } else {
             System.out.println("这是一个小数");
-            
+
         }
     }
 }
