f6e9befe11dfb038b49aa3c33633d7ae00f85541
zyly123 <13598059916@163.com>
2017-08-16 14:53:42 +0800

    zy

--- src/main/java/com/asiainfo/chapter1/zhangyang/week5_day_05/LoginServlet01.java
+++ src/main/java/com/asiainfo/chapter1/zhangyang/week5_day_05/LoginServlet01.java
@@ -1,7 +1,6 @@
 package com.asiainfo.chapter1.zhangyang.week5_day_05;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +10,7 @@ import java.io.PrintWriter;
 /**
  * Created by 11 on 2017/8/11.
  */
-@WebServlet(name = "LoginServlet01")
+
 public class LoginServlet01 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("gbk");//处理请求与响应中文字体乱码，只适用于post提交
