4b1ac6728174770305e101c76f63683c7133f7ea
zyly123 <13598059916@163.com>
2017-08-16 15:24:44 +0800

    zy

--- src/main/java/com/asiainfo/chapter1/zhangyang/week5_day_04/TestServlet.java
+++ src/main/java/com/asiainfo/chapter1/zhangyang/week5_day_04/TestServlet.java
@@ -1,7 +1,7 @@
 package com.asiainfo.chapter1.zhangyang.week5_day_04;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +11,7 @@ import java.io.PrintWriter;
 /**
  * Created by 11 on 2017/8/10.
  */
-@WebServlet( name = "TestServlet")
+
 public class TestServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String high = request.getParameter("high");
