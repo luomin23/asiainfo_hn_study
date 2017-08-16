ae729a3954252e61773f368741d7fe07b9a05b2b
HanJunwen <853104269@qq.com>
2017-08-16 09:49:25 +0800

    工程提交

---
+++ src/main/webapp/chapter1/HanJunwen/JavaWebDemo01/src/test/TestServlet01.java
@@ -0,0 +1,27 @@
+package test;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+//设置访问路径
+@WebServlet("/hello.view")
+public class TestServlet01 extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+//重新定义doGet()方法
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");//设置响应内容类型
+        PrintWriter out = response.getWriter();//取得响应输出对象
+        String name =request.getParameter("name");//取得请求参数，也就是url中传递过来的数据
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title></title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>hello!"+name+"!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+}
